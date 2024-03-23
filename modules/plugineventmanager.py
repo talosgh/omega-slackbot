db20b6b541039771d700e44fe854a8d161787b75
TJ <william@theesfeld.net>
2024-03-23 14:53:18 -0400

    remove omegaslackbot from plugin requirements

--- modules/plugineventmanager.py
+++ modules/plugineventmanager.py
@@ -7,11 +7,21 @@ class PluginEventManager:
         if event_name not in self._listeners:
             self._listeners[event_name] = []
         self._listeners[event_name].append(listener)
-        self.app.log.info(f"{listener} Subscribed to {event_name}")
+        # self.app.log.info(f"{listener} Subscribed to {event_name}")
 
     def publish(self, event_name, *args, **kwargs):
         listeners = self._listeners.get(event_name, [])
         if not listeners:
             self.app.log.info(f"No listeners for event: {event_name}")
         for listener in listeners:
+            if hasattr(listener.__self__, "name"):
+                if listener.__self__.name == "Omega Slack Bot":
+                    continue
+                listener_name = listener.__self__.name
+            else:
+                listener_name = "Unknown"
+
+            self.app.log.info(
+                f"Publishing event: {event_name} - waiting for {listener_name} to process..."
+            )
             listener(*args, **kwargs)
--- plugins/parsefile/parsefile.py
+++ plugins/parsefile/parsefile.py
@@ -3,26 +3,29 @@ import pandas as pd
 
 
 class Plugin:
+
     def __init__(self, app, config):
         self.app = app
         self.config = config
         self.name = config["name"]
         self.version = config["version"]
+        self.author = config["author"]
+        self.github = config["github"]
+        self.description = config["description"]
         self.app.event_handler.subscribe("identify", self.identify)
         self.app.event_handler.subscribe(
             "application_started", self.on_application_started
         )
         self.app.event_handler.subscribe("parse_file", self.parse_event)
-        self.app.event_handler.subscribe("identify", self.identify)
 
     def identify(self):
         self.app.event_handler.publish(
             "identify",
             name=self.name,
             version=self.version,
-            author=self.config["author"],
-            github=self.config["github"],
-            description=self.config["description"],
+            author=self.suthor,
+            github=self.github,
+            description=self.description,
         )
 
     def on_application_started(self):
